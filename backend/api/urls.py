acb0b92a8948425ad2d48ef42194d6e00e3f6bee
levisiedschlag <levisiedschlag@gmail.com>
2022-10-20 17:30:04 -0300

    api views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from api import views
+
+app_name='app_api'
 
 urlpatterns = [
     path('', views.api_home, name="api_home")
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,23 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "This is the Django API response!!!"})
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    print(request.GET, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    body = request.body
+    try:
+        data=json.loads(body)
+
+    except:
+        pass
+    print(data)
+    data['headers']=dict(request.headers)
+    data['content_type']=request.content_type
+    return JsonResponse(data)
