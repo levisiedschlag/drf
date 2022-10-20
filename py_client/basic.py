435848b2238b8a4c1ef3ac71217a8721ecd49fc4
levisiedschlag <levisiedschlag@gmail.com>
2022-10-20 13:44:21 -0300

    api app

---
+++ py_client/basic.py
@@ -0,0 +1,11 @@
+import requests
+
+
+
+endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
+
+get_response = requests.get(endpoint, json={"query":"Hello World"})
+# print(get_response.json())
+print(get_response.text)
+print(get_response.status_code)
